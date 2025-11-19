d1099672258e5c0ee0b90f04eedfbc6886ca1339
Ammaar Shareef <ammhood1602@gmail.com>
2025-11-18 21:05:51 -0500

    First YOLO model trained

--- PyScripts/yolo_test.py
+++ PyScripts/yolo_test.py
@@ -1,12 +1,13 @@
 # yolo_test.py
 
 from ultralytics import YOLO
+import torch
 
 # Load pretrained YOLOv8n model
-model = YOLO("yolov8n.pt")
+model = YOLO("runs/detect/train2/weights/best.pt")
 
 # Run inference on a test image
-results = model("Dataset/sarah.jpeg")  # results is a list of Results objects
+results = model("dataset/a1_53.png")  # results is a list of Results objects
 
 # Show results for each image
 for r in results:
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Clash-Royale-Bot
 Not really a full fledged bot, a simple helper that displays opponent elixir and possible cards in hand, which is not displayed usually.
+
+Restrictions:
+
+- Only train 10 classes as of now. 
+- User's deck can't contain any duplicates from opponents to avoid any disruption.
