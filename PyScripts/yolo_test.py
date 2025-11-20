7aa6bc134cfd94ded71b2af0ea668b4d1b4bb3a0
Ammaar Shareef <ammhood1602@gmail.com>
2025-11-19 21:01:59 -0500

    Second Retrained model (132 images total 8 classes)

--- PyScripts/yolo_test.py
+++ PyScripts/yolo_test.py
@@ -7,7 +7,7 @@ import torch
 model = YOLO("runs/detect/train2/weights/best.pt")
 
 # Run inference on a test image
-results = model("dataset/a1_53.png")  # results is a list of Results objects
+results = model("test14.png")  # results is a list of Results objects
 
 # Show results for each image
 for r in results:
